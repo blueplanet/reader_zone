129ece83ae1a6c4bef91598fb6ded2aa3af7f2f3
blueplanet <erguolinge@gmail.com>
2012-12-01 21:14:18 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReaderZone::Application.config.secret_token = 'e47e035635984d9804d850e408a9cdbcc09f4453b353420ccf175017d817809035034f6b6e39de53444f586d0918ebd6b2afd579b48bf9b54964c7a888f79c18'
